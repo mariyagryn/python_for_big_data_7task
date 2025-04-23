d67a7cdcff099d4c7bf14f7880fd6f8f020f3eef
mariyagryn <97542817+mariyagryn@users.noreply.github.com>
2025-04-23 09:36:57 +0200

    Add empty functions with docstrings

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+def input_from_console():
+    """Отримує текст від користувача з консолі."""
+    pass
+
+def read_from_file_builtin(filepath):
+    """Зчитує вміст файлу за допомогою вбудованих можливостей Python."""
+    pass
+
+def read_from_file_pandas(filepath):
+    """Зчитує вміст файлу за допомогою бібліотеки pandas."""
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,7 @@
+def output_to_console(text):
+    """Виводить текст у консоль."""
+    pass
+
+def write_to_file_builtin(filepath, text):
+    """Записує текст до файлу за допомогою вбудованих можливостей Python."""
+    pass
