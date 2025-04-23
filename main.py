2b945d975bf447187f19dec20d23257729a4aa53
mariyagryn <97542817+mariyagryn@users.noreply.github.com>
2025-04-23 09:38:34 +0200

    Connect functions in main

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from app.io.input import input_from_console, read_from_file_builtin, read_from_file_pandas
+from app.io.output import output_to_console, write_to_file_builtin
+
+
 def main():
-    pass
+    input_from_console()
+    read_from_file_builtin("example.txt")
+    read_from_file_pandas("example.csv")
+
+    output_to_console("Hello, world!")
+    write_to_file_builtin("output.txt", "Hello, world!")
+
 
 if __name__ == "__main__":
     main()
